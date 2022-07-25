407033043bc342329ef64fe739c3978acb1530db
NivethaArul <109789622+NivethaArul@users.noreply.github.com>
2022-07-25 13:13:12 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,28 @@
+package assignment;
+
+public class typecasting {
+	public static void main(String[] args){
+		float f=5.74f;
+        long l=(long)f;
+        System.out.println("Long value:" +l);
+        double d=6.06;
+        long ll=(long)d;
+        int i=(int)ll;
+        char c=(char)'i';
+        System.out.println("Double value:" +d);
+        System.out.println("Long value:" +ll);
+        System.out.println("Int value:" +i);
+        System.out.println("Char value:" +c);
+ long 12=3333333;
+ byte b=(byte)12;
+ System.out.println("long value:" +12);
+ System.out.println("Byte value:" +b);
+ 
+ int i1=100;
+ double d1=i1;
+ long 11=(long)d1;
+ System.out.println("Int value:" +i1);
+ System.out.println("Double value:" +d1);
+ System.out.println("long value:" +ll);
+	}
+ }
